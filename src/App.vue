7d11beecd98c488e33bd51701f8e865c9e5b207b
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-12-09 21:02:24 +0200

    App counter

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <counter></counter>
+    <info></info>
+  </div>
+</template>
+
+<script>
+import counter from "./components/counter";
+import info from "./components/info";
+
+export default {
+  name: 'app',
+  components: {
+    counter,
+    info
+  }
+}
+</script>
